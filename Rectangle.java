746095826771932f88e27f5ea9c613c72d36e7ab
Jaspreet Sahiwal <ritusahiwal@gmail.com>
2021-12-08 20:59:56 -0800

    Taking input for Rectangle from user

--- Rectangle.java
+++ Rectangle.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
 class Rectangle extends Shape 
 
-{
 
+
+{
+Scanner s=new Scanner(System.in);
 
 void area()
 {
  
+System.out.println("Enter the length of the rectangle");
 
-System.out.println(length*breadth);
+length=s.nextInt();
+
+System.out.println("Enter the breadth of the rectangle");
+
+breadth=s.nextInt();
+
+
+
+System.out.println("The area of t he rectangle is "+length*breadth);
 
 }
 
