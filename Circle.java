2a8df96e62e467d916ddd5de7f092e73b01324cb
jaspreetlpu <62795279+jaspreetlpu@users.noreply.github.com>
2021-12-09 00:45:14 -0800

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,19 +1,19 @@
 class Circle 
 {
 
-java.util.Scanner =new java.util.Scanner(System.in)
+java.util.Scanner s =new java.util.Scanner(System.in)
 double radius;
 
 void area()
 {
-
+radius =s.nextDouble();
 System.out.println("The area of the circle"+Math.PI*radius*radius);
 
 }
 
 void perimeter()
 {
-
+radius=s.nextDouble();
 System.out.println("The perimeter of the circle is" + 2*Math.PI*radius);
 
 }
